e714a065576951f474f5ac9f344fa84a30764f25
Filipe Bezerra <bezerrafilipe13@gmail.com>
2024-06-20 21:57:29 -0300

    finals settings of first delivery

--- Question1/bank_system/src/App.java
+++ Question1/bank_system/src/App.java
@@ -6,6 +6,7 @@ public class App {
     public static void main(String[] args) {
         BankAccount acc = new BankAccount(1000);
 
+        System.out.println("saldo inicial: " + acc.getBalance());
         Thread personOne = new Thread(new Deposit(acc, 80), "Person 1");
         Thread personTwo = new Thread(new Substract(acc, 100), "Person 2");
         Thread personThree = new Thread(new Substract(acc, 700), "Person 3");
