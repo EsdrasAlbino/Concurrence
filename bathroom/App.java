3ede82557ed5ed92103311fad36c2bd8c073136c
Filipe Bezerra <bezerrafilipe13@gmail.com>
2024-07-26 12:04:01 -0300

    modify the number of threads to 100

--- bathroom/App.java
+++ bathroom/App.java
@@ -3,7 +3,7 @@ public class App {
         Bathroom bathroom = new Bathroom();
 
         // Create and start multiple threads for men and women
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 100; i++) {
             Thread men = new Thread(new Person(i, "Homem", bathroom));
             Thread woman = new Thread(new Person(i, "Mulher", bathroom));
             men.start();
